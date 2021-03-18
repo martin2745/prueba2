53b9dc5e407cb3dce1c83ee6d36b3a9c9e8c91de
Martín <Martn@DESKTOP-PGCTOJQ>
2021-03-18 13:55:03 +0100

    He modificado el sout

--- src/hola/Hola.java
+++ src/hola/Hola.java
@@ -15,7 +15,7 @@ public class Hola {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Hola prueba2");
     }
     
 }
